17b47d3e457f11d0b7756de464714bdcdb7d5bcc
Delphindae99 <94207752+Delphindae99@users.noreply.github.com>
2024-10-27 14:35:21 +0000

    Add files via upload

---
+++ FocusBox-Arduino/FocusBox-Arduino.ino
@@ -0,0 +1,120 @@
+#include <Wire.h>
+#include <LiquidCrystal.h>
+const int potPin = A5;
+const int redLedPin = 9;
+const int greenLedPin = 8;
+const int blueLedPin = 7;
+
+int currentMillis,prevMillis;
+
+byte colorVal;
+int potValue;
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+#define SLAVE_ADDRESS 0x04
+
+volatile boolean receiveFlag = false;
+char temp[32];
+String command;
+
+void setup(){
+  Wire.begin(SLAVE_ADDRESS);
+
+  Wire.onReceive(receiveEvent);
+
+  Serial.begin(9600);
+  Serial.println("Ready!");
+
+  lcd.begin(16, 2);
+
+  pinMode(redLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(blueLedPin, OUTPUT);
+  analogWrite (redLedPin, 0);
+  analogWrite(greenLedPin, 0);
+  analogWrite(blueLedPin, 0);
+}
+
+void loop() {
+  if (receiveFlag == true) {
+    //Serial.println(temp);
+    if(strcmp(temp, "END!")==0){
+      lcd.clear();
+      lcd.print("Ending");
+      delay(1000);
+      lcd.clear();
+    analogWrite (redLedPin, 0);
+    analogWrite(greenLedPin, 0);
+    analogWrite(blueLedPin, 0);
+    while(true){} //kill program
+    }
+     
+    
+    
+    int month = (temp[0]-'0')*10 + (temp[1]-'0');
+    int day = (temp[3]-'0')*10 + (temp[4]-'0');
+    int year = (temp[6]-'0')*1000+(temp[7]-'0')*100+(temp[8]-'0')*10+(temp[9]-'0');
+
+    int hour = (temp[11]-'0')*10 + (temp[12]-'0');
+    if (hour == 0){
+      hour = 12;
+    } else if (hour > 12){
+      hour -= 12;
+    }
+    int minute = (temp[14]-'0')*10 + (temp[15]-'0');
+
+    lcd.clear();
+    lcd.print(month);
+    lcd.print("/");
+    lcd.print(day);
+    lcd.print("/");
+    lcd.print(year);
+    lcd.setCursor(0,1);
+    lcd.print(", ");
+    lcd.print(hour);
+    lcd.print(":");
+    lcd.println(minute);
+
+    
+    
+    receiveFlag = false;
+  }
+
+  writeLED();
+}
+
+void receiveEvent(int howMany) {
+  for (int i = 0; i < howMany; i++){
+    temp[i] = Wire.read();
+    temp[i+1] = '\0';
+  }
+
+  for (int i = 0; i < howMany; ++i) temp[i] = temp[i+1];
+  Wire.write(colorVal);
+
+  receiveFlag = true;
+}
+
+void writeLED (){
+  int rawValue = analogRead(potPin);
+  Serial.println(rawValue);
+  colorVal = map(rawValue, 0, 1023, 0, 255);
+  int wheelPos = 255 - colorVal;
+  Serial.println(wheelPos);
+  if (wheelPos < 85){
+    analogWrite(redLedPin, 255 - wheelPos *3);
+    analogWrite(greenLedPin, 0);
+    analogWrite(blueLedPin, wheelPos * 3);
+  } else if (wheelPos < 170){
+    wheelPos -= 85;
+    analogWrite(redLedPin, 0);
+    analogWrite(greenLedPin, wheelPos *3);
+    analogWrite(blueLedPin, 255 - wheelPos *3);
+  } else {
+    wheelPos -= 170;
+    analogWrite(redLedPin, wheelPos * 3);
+    analogWrite(greenLedPin, 255 - wheelPos * 3);
+    analogWrite(blueLedPin, 0);
+  }
+}
---
+++ FocusBox-RPi.py
@@ -0,0 +1,99 @@
+import datetime
+import smbus
+import time
+import RPi.GPIO as GPIO
+import pygame
+
+pygame.mixer.init()
+pygame.init()
+
+
+GPIO.setmode(GPIO.BCM)
+closePin = 23
+
+bus = smbus.SMBus(1)
+time.sleep(1)
+
+prevTime = 0
+
+arduinoAddress = 0x04
+
+GPIO.setup(closePin, GPIO.IN, GPIO.PUD_UP)
+
+def writeData(value):
+    byteValue = StringToBytes(value)
+    bus.write_i2c_block_data(arduinoAddress,0x00,byteValue)
+    return -1
+
+def StringToBytes(val):
+    retVal = []
+    for c in val:
+        retVal.append(ord(c))
+    return retVal
+
+
+class Fader(object):
+    instances = []
+    def __init__(self, fname):
+        super(Fader, self).__init__()
+        #assert isinstance(fname, basestring)
+        self.sound = pygame.mixer.Sound(fname)
+        self.increment = 0.01 # tweak for speed of effect!!
+        self.next_vol = 1 # fade to 100 on start
+        Fader.instances.append(self)
+
+    def fade_to(self, new_vol):
+        # you could change the increment here based on something..
+        self.next_vol = new_vol
+
+    @classmethod
+    def update(cls):
+        for inst in cls.instances:
+            curr_volume = inst.sound.get_volume()
+            # print inst, curr_volume, inst.next_vol
+            if inst.next_vol > curr_volume:
+                inst.sound.set_volume(curr_volume + inst.increment)
+            elif inst.next_vol < curr_volume:
+                inst.sound.set_volume(curr_volume - inst.increment)
+
+sound1 = Fader("piano_music.wav")
+sound2 = Fader("synth.wav")
+sound1.sound.play()
+sound2.sound.play()
+sound2.sound.set_volume(0)
+
+# fading..
+sound1.fade_to(0)
+sound2.fade_to(1)
+
+
+ledColor = 0
+try:
+    while True:
+        currentSeconds = time.time()
+        isSaving = False;
+        if GPIO.input(closePin) == 0:
+            time.sleep(.03)
+            if GPIO.input(closePin) == 0:
+                while GPIO.input(closePin) == 0:
+                    time.sleep(0.02);
+                raise KeyboardInterrupt
+        Fader.update
+        if currentSeconds - prevTime > 60.0 or isSaving:
+            currentTime = datetime.datetime.now()
+            timeString = str(currentTime.month)+" "+str(currentTime.day)+" "+str(currentTime.year)+" "+str(currentTime.hour).zfill(2)+" "+str(currentTime.minute).zfill(2)
+            print(timeString)
+            if isSaving:
+                print("placeholder")
+            print("Sending")
+            writeData(timeString)
+            ledColor = bus.read_byte_data(arduinoAddress, 0)
+            print(ledColor);
+            prevTime = currentSeconds
+            #time.sleep(60)
+except KeyboardInterrupt:
+    print("Ending program")
+    writeData("END!")
+finally:
+    GPIO.cleanup()
+    pygame.quit()
